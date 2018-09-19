87386ca4e505f4487b965e3445639d7041e95d80
liuhuobing <619102150@qq.com>
2018-09-19 16:52:35 +0800

    关键词

---
+++ src/180919-key-word.js
@@ -0,0 +1,17 @@
+/*
+ * 以下为js关键词
+ */
+let t = 'arguments、break、case、catch、class、const、continue、' +
+    'debugger、default、delete、do、else、enum、eval、export、extends、' +
+    'false、finally、for、function、if、implements、import、in、instanceof、' +
+    'interface、let、new、null、package、private、protected、public、return、static、' +
+    'super、switch、this、throw、true、try、typeof、var、void、while、with、yield';
+
+let a = t.split('、');
+
+for (let i = 0; i < a.length; ) {
+    console.log(a.slice(i, i+5).join('\t'));
+    i += 5;
+}
+
+
